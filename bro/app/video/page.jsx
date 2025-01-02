1a453ef7bb09b868c90c288c59f4d74756a89bd2
Aaryan Bansal <aaryan.bansal.dev@gmail.com>
2025-01-02 10:12:45 +0000

    Video

--- bro/app/video/page.jsx
+++ bro/app/video/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
       <h1 className="py-[15] font-bold text-2xl text-slate-300 px-24">How BRO Works (Video)</h1>
-      <center><iframe src="https://www.youtube.com/embed/JCVnMgumyDg" allowFullScreen className=''/></center><br /><br /><br />
+      <center><iframe src="https://www.youtube.com/embed/QAkP8ndBo24" allowFullScreen className=''/></center><br /><br /><br />
     </div>
   )
 }
